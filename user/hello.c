7e2c791a7ab4bb38a6a8336eadbed26f47cb33cd
Ashton Koehl <koehl@unlv.nevada.edu>
2025-02-02 21:43:13 -0800

    my solution for hello xv6

---
+++ user/hello.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+    printf("Hello world, this is Ashton Koehl.\n");
+    printf("I am in CS 370, Spring 2025, Section 1002.\n\n");
+
+    exit(0);
+}
